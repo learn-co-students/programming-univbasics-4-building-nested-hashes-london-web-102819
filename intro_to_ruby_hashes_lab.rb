621852bffe476eeaf0440155d03bd4e810be7032
Anastasia Pankina <siapankina@gmail.com>
2019-09-17 17:54:47 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -8,15 +8,10 @@ def base_hash
 end
 
 def monopoly_with_second_tier
-  monopoly={:railroads=>{}}
-  a={
-    :pieces=>
-    
-  }
-  monopoly[railroads]=monopoly[railroads].merge(a[pieces])
-  return monopoly
+  monopoly = base_hash();
+  monopoly[:railroads][:pieces] = 4;
   
-
+  return monopoly;
   
   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
@@ -25,6 +20,11 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
+  monopoly=monopoly_with_second_tier();
+  monopoly[:railroads][:rent_in_dollars]={:one_piece_owned=> 25,:two_pieces_owned=> 50, :three_pieces_owned=> 100, :four_pieces_owned=> 200};
+  monopoly[:railroads][:names]={:reading_railroad=>{},:pennsylvania_railroad=>{}, :b_and_o_railroad=>{}, :shortline_railroad=>{}};
+  
+  return monopoly;
   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
@@ -32,6 +32,14 @@ def monopoly_with_third_tier
 end
 
 def monopoly_with_fourth_tier
+  monopoly=monopoly_with_third_tier();
+  monopoly[:railroads][:names][:reading_railroad]["mortgage_value"]="$100";
+   monopoly[:railroads][:names][:pennsylvania_railroad]["mortgage_value"]="$200";
+    monopoly[:railroads][:names][:b_and_o_railroad]["mortgage_value"]="$400";
+    monopoly[:railroads][:names][:shortline_railroad]["mortgage_value"]="$800";
+  
+  
+  return monopoly;
   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
